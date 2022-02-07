f4ae8f8b8cd3e358b2c56b03991d98f8a9655b8a
oscar <oscar@DESKTOP-SCPSO2U>
2022-02-07 17:53:07 -0300

    Add project files.

---
+++ Exercicio01/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Exercício 1:
+• Faça um algoritmo que leia os valores A, B, C e imprima na tela se a
+soma de A + B é menor que C.*/
+
+using System;
+
+namespace Exercicio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor A: ");
+            double valorA = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o valor B: ");
+            double valorB = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o valor C: ");
+            double valorC = Convert.ToDouble(Console.ReadLine());
+
+            double somaAB = valorA + valorB;
+
+            if (somaAB > valorC)
+            {
+                Console.WriteLine($"A soma de A + B ({somaAB}) é maior que o valor C ({valorC})");
+            }
+            else Console.WriteLine($"A soma de A + B ({somaAB}) é menor que o valor C ({valorC})");
+        }
+    }
+}
---
+++ Exercicio02/Program.cs
@@ -0,0 +1,76 @@
+﻿/*Exercício 2:
+• Escreva um algoritmo que leia três valores inteiros e diferentes e
+mostre-os em ordem decrescente.*/
+
+using System;
+
+namespace Exercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("\nValor A: ");
+                int valorA = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Valor B: ");
+                int valorB = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Valor C: ");
+                int valorC = Convert.ToInt32(Console.ReadLine());
+
+                if (valorA == valorB || valorA == valorC || valorC == valorB)
+                {
+                    Console.WriteLine("\nOs Valores A, B e C devem ser diferentes.\nTente novamente.");
+                    continue;
+                }
+
+                int maiorNumero = 0;
+
+                if (valorA > valorB && valorA > valorC)
+                {
+                    maiorNumero = valorA;
+                }
+                else if (valorB > valorC && valorB > valorA)
+                {
+                    maiorNumero = valorB;
+                }
+                else if (valorC > valorA && valorC > valorB)
+                {
+                    maiorNumero = valorC;
+                }
+
+                int segundoNumero = 0;
+
+                if (valorA < valorB && valorA > valorC || valorA < valorC && valorA > valorB)
+                {
+                    segundoNumero = valorA;
+                }
+                else if (valorB > valorC && valorB < valorA || valorB > valorA && valorB < valorC)
+                {
+                    segundoNumero = valorB;
+                }
+                else if (valorC > valorA && valorC < valorB || valorC > valorB && valorC < valorA)
+                {
+                    segundoNumero = valorC;
+                }
+
+                int terceiroNumero = 0;
+
+                if (valorA < valorB && valorA < valorC)
+                {
+                    terceiroNumero = valorA;
+                }
+                else if (valorB < valorA && valorB < valorC)
+                {
+                    terceiroNumero = valorB;
+                }
+                else terceiroNumero = valorC;
+
+                Console.WriteLine($"{maiorNumero}, {segundoNumero}, {terceiroNumero}.");
+            }
+        }
+    }
+}
